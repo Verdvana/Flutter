b55bc65f3e7ae2b36c1b72c33525b706c62db688
verdvana <739197909@qq.com>
2026-05-01 15:06:36 +0800

    Initial Commit

---
+++ Flutter/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Flutter
+//
+//  Created by VERDVANA on 2026/5/1.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
